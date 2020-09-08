9f4ffc2701920463be1075a7a2734553d4c319e8
Mighty Chip <mighty.chip@gmail.com>
2020-09-08 11:08:49 -0400

    Cloud config post 1 (#1)
    * Run configurations have been added for the two docker compositions (prod and dev).  Additionally, things have been cleaned up and commented a bunch. An HTTP Request file is included for testing some of the endpoints.
    
    * Final cleanup, adding some more Run Configurations, and making sure the Docker images are now built for a local registry instead.

--- example-client/src/main/java/ca/purpleowl/examples/spring/cloud/config/client/configuration/PropertiesConfiguration.java
+++ example-client/src/main/java/ca/purpleowl/examples/spring/cloud/config/client/configuration/PropertiesConfiguration.java
@@ -4,6 +4,9 @@ import ca.purpleowl.examples.spring.cloud.config.client.configuration.properties
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Enable configuration properties and specify our Configuration Properties beans... well, bean in this case I guess.
+ */
 @Configuration
 @EnableConfigurationProperties(PronounCheckProperties.class)
 public class PropertiesConfiguration {
--- example-client/src/main/java/ca/purpleowl/examples/spring/cloud/config/client/controller/HelloController.java
+++ example-client/src/main/java/ca/purpleowl/examples/spring/cloud/config/client/controller/HelloController.java
@@ -2,17 +2,23 @@ package ca.purpleowl.examples.spring.cloud.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * This controller illustrates direct retrieval of a value from the Cloud Config properties...
+ *
+ * Notice that when we use the {@link Value} annotation, changes from a refreshed Application Context (ie. when values
+ * from the Cloud Config Server have changed) are not seeded.  This is an undesirable design trait, as it requires a
+ * full application restart.
  */
 @RestController
+@RequestMapping("/message")
 public class HelloController {
     @Value("${example-client.message:Default hello message}")
     private String message;
 
-    @GetMapping("/message")
+    @GetMapping
     public String getMessage() {
         return message;
     }
